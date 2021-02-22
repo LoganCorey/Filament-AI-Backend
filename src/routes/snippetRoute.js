87aa9164ce1c1b5d99b2eb7596d37fc6b470385a
Logan Corey <38678360+LoganCorey@users.noreply.github.com>
2021-02-22 00:36:14 -0500

    Added catch all route

--- src/routes/snippetRoute.js
+++ src/routes/snippetRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const snippetController = require('../controllers/snippetController');
 const authController = require('../controllers/authController');
+const AppError = require('../lib/appError');
 
 const router = express.Router();
 
@@ -14,5 +15,9 @@ router
   .get(authController.protect, snippetController.getById)
   .patch(authController.protect, snippetController.patchById)
   .delete(authController.protect, snippetController.deleteById);
+
+  router.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl}`, 404));
+  });
 
 module.exports = router;
