deec082427e6c4a881a79a536fdb5d416374e80d
Logan Corey <38678360+LoganCorey@users.noreply.github.com>
2021-02-22 10:52:26 -0500

    Updated error handling for unique violation errors

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -3,6 +3,7 @@
  * sense to place this logic here since this is
  * where the error logic will be controlled
  */
+
 const AppError = require('../lib/appError');
 
 const sendErrorDev = (err, res) => {
@@ -58,40 +59,47 @@ handleNotFoundError = (err) => {
 const handleJWTError = (err) => new AppError('Invalid token.  Please login in again!', 401);
 const handleJWTExpiredError = (err) => new AppError('Your token has expired.  Please log in again!', 401);
 
-const handleUniqueViolationError =(err)=>new AppError('The fields entered are not unique!', 401);
+const handleUniqueViolationError =(err)=>{
+  const message = `${err.columns.join(" ")} is already in use`
+  return new AppError(message, 401)
+};
 
 module.exports = (err, req, res, next) => {
   // if not defined internal server error
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
   // if we're in development send all the data for debugging
+ 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
     error.message = err.message
     // missing fields for post requests
-    if (error.name === 'NotNullViolationError') {
-      error = handleNullErrorDB(error);
-    }
-    // not found resource
-    if (error.name === 'NotFoundError') {
-      error = handleNotFoundError(error);
-    }
-    if(error.name === 'UniqueViolationError'){
-      error = handleUniqueViolationError(err);
-    }
-    // incorrect parameter for id in get requests
-    if (error.name === 'DataError') {
-      error = handleDataErrorDB(error, req);
-    }
-    // for expired/modified tokens
-    if (error.name === 'JsonWebTokenError') {
-      error = handleJWTError(error);
-    }
-    if (error.name === 'TokenExpiredError') {
-      error = handleJWTExpiredError(error);
-    }
+
+      if (error.name === 'NotNullViolationError') {
+        error = handleNullErrorDB(error);
+      }
+      // not found resource
+      if (err.name === 'NotFoundError') {
+        error = handleNotFoundError(error);
+      }
+      if(err.name === 'UniqueViolationError'){
+        error = handleUniqueViolationError(err);
+      }
+      // incorrect parameter for id in get requests
+      if (err.name === 'DataError') {
+        error = handleDataErrorDB(error, req);
+      }
+      // for expired/modified tokens
+      if (err.name === 'JsonWebTokenError') {
+        error = handleJWTError(error);
+      }
+      if (err.name === 'TokenExpiredError') {
+        error = handleJWTExpiredError(error);
+      }
+    
+
 
     sendErrorProd(error, res);
   }
